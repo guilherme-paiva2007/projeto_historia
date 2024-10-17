7095726633957d6b11debf41ae9926bc934f43a5
lannakamilly <lannakamillyfresmota@gmail.com>
2024-10-17 15:43:38 -0300

    lanna

---
+++ home.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="./css/style.css"/>
+    <title>Document</title>
+</head>
+<body>
+<nav>
+   
+      </div>
+      <ul class="nav__links" id="nav-links">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Glossário</a></li>
+      </ul>
+      <div class="nav__btns">
+        <button class="btn">Sign Up</button>
+      </div>
+    </nav>
+    <header class="section__container header__container">
+      <div class="header__image">
+      <img src="../css/3079185.png" alt="">
+      </div>
+      <div class="header__content">
+        <h2>Bem vindo a</h2>
+        <h1>Crônicas do tempo</h1>
+       
+      </div>
+    </header>
+
+</body>
+</html>
