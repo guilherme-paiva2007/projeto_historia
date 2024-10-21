e6d038426fb19cec67a091a60361d1326f8cf3ad
lannakamilly <lannakamillyfresmota@gmail.com>
2024-10-20 21:42:54 -0300

    commit home

--- home.php
+++ home.php
@@ -1,38 +1,270 @@
-<?php
-    $base_project_name = '/projeto_historia';
-    $root_folder = $_SERVER['DOCUMENT_ROOT'] . $base_project_name;
-    $root_folder_link = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . $base_project_name;
-    include $root_folder . '/php/functions.php';
-    include $root_folder . '/php/connection.php';
-    session_start();
-?>
+
+
+
 <!DOCTYPE html>
-<html lang="pt-br">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="./css/style.css"/>
-        <title>Document</title>
-    </head>
-    <body>
-        <nav>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/remixicon@4.3.0/fonts/remixicon.css"
+      rel="stylesheet"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
+    />
+    <link rel="stylesheet" href="./css/home.css"/>
+    <title>Amor por história</title>
+  </head>
+  <body>
+    <nav>
+      <div class="nav__header">
+        <div class="logo">
+        <img src="./img/logo.jpeg" alt="">
+        </div>
+       
+      </div>
+      <ul class="nav__links" id="nav-links">
+        <li><a href="#home">HOME</a></li>
+        <li><a href="#about">GLOSSÁRIO</a></li>
+        <li><a href="#about">GLOSSÁRIO</a></li>
+      </ul>
+      <div class="nav__btns">
+        <button class="btn">Sair</button>
+      </div>
+    </nav>
+
+    <header id="home">
+      <div class="header__container">
+        <div class="header__content">
+          <p>DESBRAVANDO O PASSADO</p>
+          <h1>Bem-vindo ao seu portal de conhecimento.</h1>
+          <div class="header__btns">
+            
+          </div>
+        </div>
+        <div class="header__image">
+        <img src="./img/lupa.png" alt="">
+        </div>
+      </div>
+    </header>
+
+    <section class="section__container destination__container" id="about">
+      <h2 class="section__header">Cronologia da História</h2>
+      <div class="destination__grid">
+        <div class="destination__card">
+        <a href="pagina_idade_contemporanea.html">
+        <img src="./img/prehistoria.webp" alt="destination" /></a>
+          <div class="destination__card__details">
+            <div>
+              <h4>Pré-História</h4>
             </div>
-            <ul class="nav__links" id="nav-links">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Glossário</a></li>
-            </ul>
-            <div class="nav__btns">
-                <button class="btn">Sign Up</button>
+          </div>
+        </div>
+
+         <div class="destination__card">
+         <a href="pagina_idade_contemporanea.html">
+        <img src="./img/teste.jpeg" alt="destination" /> </a>
+          <div class="destination__card__details">
+            <div>
+              <h4>Idade Antiga</h4>
             </div>
-        </nav>
-        <header class="section__container header__container">
-            <div class="header__image">
-                <img src="../css/3079185.png" alt="">
+          </div>
+        </div> 
+        
+        <div class="destination__card">
+        <a href="pagina_idade_contemporanea.html">
+        <img src="./img/media.jpeg" alt="destination" />
+        </a>
+          <div class="destination__card__details">
+            <div>
+              <h4>Idade Média</h4>
             </div>
-            <div class="header__content">
-                <h2>Bem vindo a</h2>
-                <h1>Crônicas do tempo</h1>
-            </div>
-        </header>
-    </body>
+          </div>
+        </div>
+      </div>
+
+      <div class="destination__grid">
+      <div class="destination__card">     
+      <a href="pagina_idade_moderno.html">
+        <img src="./img/moder.jpeg" alt="destination" />
+      </a>
+      <div class="destination_card_details">
+        <div>
+          <p>ㅤ</p>
+          <h3>ㅤIdade Moderna</h3>
+        </div>
+      </div>
+    </div>
+
+    
+    <div class="destination__card">
+      <a href="pagina_idade_contemporanea.html">
+        <img src="./img/comte.jpeg" alt="destination" />
+      </a>
+      <div class="destination_card_details">
+        <div>
+        <p>ㅤ</p>
+          <h3>ㅤIdade Contemporânea</h3>
+        </div>
+      </div>
+    </div>
+  </div>
+    </section>
+
+    <section class="section__container journey__container" id="tour">
+      <h2 class="section__header">Explore os Caminhos da História!</h2>
+      <p class="section__description">
+      Descubra os eventos e personagens que moldaram o mundo.
+      </p>
+      <div class="journey__grid">
+        <div class="journey__card">
+          <div class="journey__card__bg">
+            <span><i class="ri-bookmark-3-line"></i></span>
+            <h4>Momentos Decisivos</h4>
+          </div>
+          <div class="journey__card__content">
+            <span><i class="ri-bookmark-3-line"></i></span>
+            <h4>Crises, Revoluções e Vitórias</h4>
+            <p>
+            Explore os momentos em que a história mudou drasticamente o rumo da humanidade.
+            </p>
+          </div>
+        </div>
+        <div class="journey__card">
+          <div class="journey__card__bg">
+            <span><i class="ri-landscape-fill"></i></span>
+            <h4>Civilizações Antigas</h4>
+          </div>
+          <div class="journey__card__content">
+            <span><i class="ri-landscape-fill"></i></span>
+            <h4>Egito, Roma e Além</h4>
+            <p>
+            Descubra como as primeiras grandes civilizações influenciaram o mundo moderno.
+            </p>
+          </div>
+        </div>
+        <div class="journey__card">
+          <div class="journey__card__bg">
+            <span><i class="ri-map-2-line"></i></span>
+            <h4>Personagens Notáveis</h4>
+          </div>
+          <div class="journey__card__content">
+            <span><i class="ri-map-2-line"></i></span>
+            <h4>Líderes, Visionários e Heróis</h4>
+            <p>
+            Conheça as figuras mais influentes da história e seus legados.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="section__container showcase__container" id="package">
+      <div class="showcase__image">
+      <img src="./img/teste3.jpg" alt="destination" />
+      </div>
+      <div class="showcase__content">
+        <h4>Aprenda História de Forma Dinâmica</h4>
+        <p>
+        Este site foi criado como uma ferramenta interativa para auxiliar os alunos no aprendizado de história. Desenvolvido em conjunto com profissionais de desenvolvimento de sistemas, ele oferece um espaço digital onde é possível explorar conteúdos históricos de maneira acessível e envolvente, complementando o que é ensinado em sala de aula.
+        </p>
+        <p>
+        Com este recurso, os alunos podem aprofundar seus conhecimentos, praticar para avaliações e reforçar o entendimento de momentos e figuras históricas. O site é pensado para tornar o estudo da história mais interessante e dinâmico, permitindo uma imersão nos fatos e processos que moldaram o mundo atual.
+        </p>
+      </div>
+    </section>
+
+    <section class="section__container discover__container">
+      <h2 class="section__header">A influência da história em nosso mundo</h2>
+      <p class="section__description">
+
+      </p>
+      <div class="discover__grid">
+        <div class="discover__card">
+        <span><i class="ri-ship-line"></i></span>
+          <h4>Conquistas e impérios</h4>
+          <p>
+          Impérios dominaram regiões e moldaram culturas, espalhando ideias e tecnologias que marcaram a história de várias civilizações.
+          </p>
+        </div>
+        <div class="discover__card">
+        <span><i class="ri-camera-lens-line"></i></span>
+          
+          <h4>Arte e cultura</h4>
+          <p>
+          A arte e cultura ao longo da história refletiram valores e influenciaram a forma como as sociedades se expressam e se relacionam.
+          </p>
+        </div>
+        <div class="discover__card">
+          <span><i class="ri-landscape-line"></i></span>
+          <h4>Ciência e história</h4>
+          <p>
+          Inovações científicas e tecnológicas moldaram a vida humana, transformando desde o cotidiano até o desenvolvimento das sociedades.
+          </p>
+        </div>
+      </div>
+    </section>
+
+
+    <footer id="contact">
+      <div class="section__container footer__container">
+        <div class="footer__col">
+          <div class="footer__logo">
+            <a href="#" class="logo">Sesi</a>
+          </div>
+          <p>
+          A Educação Básica do SESI-SP é baseada em uma proposta pedagógica sólida, focada na formação cidadã. Seu objetivo é capacitar os alunos para participarem ativamente em uma sociedade diversa, promovendo liberdade, respeito e solidariedade.
+          </p>
+          <ul class="footer__socials">
+            <li>
+              <a href="https://www.facebook.com/share/XNEfQuartHJrPeNq/"><i class="ri-facebook-fill"></i></a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com/sesi.cpv?igsh=MW94aGQ4OHNhNTE1bw=="><i class="ri-instagram-line"></i></a>
+            </li>
+          </ul>
+        </div>
+        <div class="footer__col">
+          <h4>Integrantes</h4>
+          <ul class="footer__links">
+          <li><a href="https://www.instagram.com/l1nn_l?igsh=MWVscHA2YWRiZWtoeQ==" target="_blank">Guilherme Paiva</a></li>
+    <li><a href="https://www.instagram.com/lanna.kfm?igsh=MW83enltcjZ3czJuOQ==" target="_blank">Lanna Kamilly</a></li>
+    <li><a href="https://www.instagram.com/flaviaglendaa_?igsh=NWd6NWpkdG8yejF3" target="_blank">Flávia Glenda</a></li>
+    <li><a href="https://www.instagram.com/borgesm58?igsh=azI0MHdoYW92cDc=" target="_blank">Miguel Borges</a></li>
+          </ul>
+        </div>
+        <div class="footer__col">
+          <h4>Contato</h4>
+          <ul class="footer__links">
+            <li>
+              <a href="#">
+                <span><i class="ri-phone-fill"></i></span> (12) 3653-1943
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <span><i class="ri-map-pin-2-fill"></i></span> Sesi Caçapava, 207
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="footer__col">
+          <h4>Deixe seu feedback do site !</h4>
+          <form action="/">
+            <input type="text" placeholder="Escreva..." />
+            <button class="btn">Enviar</button>
+          </form>
+        </div>
+      </div>
+      <div class="footer__bar">
+        Sesi Senai © 2024 Desenvolvimento de Sistemas.
+      </div>
+    </footer>
+<!-- nâo apagar, js responsável por transição -->
+    <script src="https://unpkg.com/scrollreveal"></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <script src="./js/home.js"></script>
+  </body>
 </html>
