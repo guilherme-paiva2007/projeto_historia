99177e962c0ab24a2b3a4f77a14092de61082376
miguel_borges <miguelborgess2016@gmail.com>
2024-10-31 13:46:41 -0300

    CADASTRO

--- login.php
+++ login.php
@@ -34,17 +34,23 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <?php include './html/links.php'; ?>
+        <?php include './html/links.php'; ?> 
+        <link rel="stylesheet" href="./css/logim.css">
 
         <title>Formulário</title>
     </head>
     <body>
         <form action="" method="post">
+            <section>
+                <h2>Conectar-se</h2>
+            </section>
             <label for="input-email">E-mail:</label>
             <input type="email" id="input-email" name="email"><br><br>
 
             <label for="input-password">Senha:</label>
             <input type="password" id="input-password" name="password"><br><br>
+
+            <a id="a_login" href="./cadastro.php">Criar conta</a>
 
             <input type="submit" value="Enviar">
         </form>
