4af17aac218e31f89b15cc244f7eb93b74635056
flaviaglenda <flaviaglenda15@gmail.com>
2024-11-07 15:42:21 -0300

    animaçao avaliacao

--- home.php
+++ home.php
@@ -288,7 +288,7 @@ error_reporting(0); ini_set("display_errors", 0 );
             ensinado em sala de aula.
         </p>
         <p>
-            Com este recurso, os alunos podem aprofundar seus conhecimentos, praticar para avaliações e reforçar o
+            Com este recurso, os alunos podem aprofundar seus conhecimentos, estudar para avaliações e reforçar o
             entendimento de momentos e figuras históricas. O site é pensado para tornar o estudo da história mais
             interessante e dinâmico, permitindo uma imersão nos fatos e processos que moldaram o mundo atual.
         </p>
@@ -367,10 +367,53 @@ error_reporting(0); ini_set("display_errors", 0 );
         </div>
         <div class="footer__col">
             <h4>Deixe seu feedback do site !</h4>
-            <form action="/">
-                <input type="text" placeholder="Escreva..." />
-                <button class="btn">Enviar</button>
-            </form>
+<div class="radio">
+  <input id="rating-5" type="radio" name="rating" value="5" />
+  <label for="rating-5" title="5 stars">
+    <svg viewBox="0 0 576 512" height="1em" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"
+      ></path>
+    </svg>
+  </label>
+
+  <input id="rating-4" type="radio" name="rating" value="4" />
+  <label for="rating-4" title="4 stars">
+    <svg viewBox="0 0 576 512" height="1em" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"
+      ></path>
+    </svg>
+  </label>
+
+  <input id="rating-3" type="radio" name="rating" value="3" />
+  <label for="rating-3" title="3 stars">
+    <svg viewBox="0 0 576 512" height="1em" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"
+      ></path>
+    </svg>
+  </label>
+
+  <input id="rating-2" type="radio" name="rating" value="2" />
+  <label for="rating-2" title="2 stars">
+    <svg viewBox="0 0 576 512" height="1em" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"
+      ></path>
+    </svg>
+  </label>
+
+  <input id="rating-1" type="radio" name="rating" value="1" />
+  <label for="rating-1" title="1 star">
+    <svg viewBox="0 0 576 512" height="1em" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"
+      ></path>
+    </svg>
+  </label>
+</div>
+
         </div>
     </div>
     <div class="footer__bar">
