aebf2b572b827c9653c97e763bc5c3e78132da13
Miguelb158 <miguelborgess2016@gmail.com>
2024-11-21 02:18:39 -0300

    manuel

---
+++ js/manual.js
@@ -0,0 +1,6 @@
+function exibemanu() {
+    Swal.fire({
+        title: "Como jogar?",
+        text: "Esse é o jogo FORCA, seu objetivo é tentar descobrir a palava escondida colocando letras que você acha que estão presentes nessa palavra. Porem tome cuidado, você não pode colocar mais de 6 letras erradas, se isso acontecer você perderá. Para facilitar as coisas você pode olhara a dica que esta debaixo do teclado. Boa sorte!",
+      });
+}
